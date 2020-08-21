67bb09835fc8c5feab6c9c830d4ae203dc442e35
Aaron Griffith <aargri@gmail.com>
2020-08-21 15:59:06 -0400

    added long example archive

--- src/examples.rs
+++ src/examples.rs
@@ -1,8 +1,14 @@
 #[cfg(test)]
-pub const EXAMPLES: &[(&[u8], &[(&str, &[u8])])] = &[(
-    include_bytes!("examples/demo.z"),
-    &[
-        ("hello.txt", b"Hello, world!"),
-        ("subdir\\test.txt", b"fnord"),
-    ],
-)];
+pub const EXAMPLES: &[(&[u8], &[(&str, &[u8])])] = &[
+    (
+        include_bytes!("examples/demo.z"),
+        &[
+            ("hello.txt", b"Hello, world!"),
+            ("subdir\\test.txt", b"fnord"),
+        ],
+    ),
+    (
+        include_bytes!("examples/undhr.z"),
+        &[("undhr.md", include_bytes!("examples/undhr.md"))],
+    ),
+];
